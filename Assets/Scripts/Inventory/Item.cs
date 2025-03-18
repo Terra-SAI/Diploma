f8c87ef927e583e39d4484588a1dbc2f5eea4094
Anna Stepanchikova <ann.brown.from.the.nightmare@gmail.com>
2025-03-18 11:02:18 +0300

    Inventory beggining

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string nameItem;
+    public int id;
+    public int countItem;
+    public bool isStackable;
+    [Multiline(5)]
+    public string descriptionIten;
+
+    public string pathIcon;
+    public string pathPrefab;
+}
