b42a2eec0834d2393646bc58a4d1d5e7c25fc820
Anna Stepanchikova <ann.brown.from.the.nightmare@gmail.com>
2025-04-18 02:49:40 +0300

    socks done

--- Assets/Scripts/Socks/dresserItem.cs
+++ Assets/Scripts/Socks/dresserItem.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class dresserItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool canActivate = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            canActivate = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            canActivate = false;
+        }
     }
 }
