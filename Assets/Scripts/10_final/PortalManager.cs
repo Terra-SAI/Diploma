0bf3ce23fdef8c5f86b7509e8d346dee7a2380df
Anna Stepanchikova <ann.brown.from.the.nightmare@gmail.com>
2025-05-06 02:17:23 +0300

    on and off

---
+++ Assets/Scripts/10_final/PortalManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalManager : MonoBehaviour
+{
+    [Space] public InventoryManager inventoryManager;
+    [SerializeField] private List<int> requiredItemIds = new List<int> { 101, 102, 103, 104 };
+
+    [Space]
+    [SerializeField] private GameObject mainCamera;
+
+    [Space]
+    [SerializeField] private GameObject altar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        altar.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inventoryManager.HasItemsWithIds(requiredItemIds))
+        {
+            altar.gameObject.SetActive(true);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            { 
+                var commItem = hit.collider.GetComponent<PortItem>();
+
+                if (commItem != null)
+                {
+                    if (inventoryManager.HasItemsWithIds(requiredItemIds))
+                    {
+                        SaveManager.Instance.ResetSave();
+                        SceneManager.LoadScene("00_Menu");
+                    }
+                    else 
+                    { 
+                        commItem.ActivateComment();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/9_believe/Believer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Believer : MonoBehaviour
+{
+    [SerializeField] private int normalParam = 30;
+    public void DoBelieve()
+    {
+        SaveManager.Instance.AddToProgress(normalParam);
+        SaveManager.Instance.SaveGame("10_Final", SaveManager.Instance.GetProgress());
+        SceneManager.LoadScene("10_Final");
+    }
+
+    public void DoNotBelieve()
+    {
+        SaveManager.Instance.AddToProgress(-normalParam);
+        SaveManager.Instance.SaveGame("10_Final", SaveManager.Instance.GetProgress());
+        SceneManager.LoadScene("10_Final");
+    }
+}
